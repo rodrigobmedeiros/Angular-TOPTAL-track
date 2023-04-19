44685b621d79511f96192c32d744ae64298cb9f6
Rodrigo Bernardo Medeiros <rbmedeiros@lan.esss.co>
2023-04-19 06:21:39 -0300

    Solve exercise creating components from scratch

--- angular-the-complete-guide-2023/My First Project/my-first-app/src/app/app.module.ts
+++ angular-the-complete-guide-2023/My First Project/my-first-app/src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ServerComponent } from './server/server.component';
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './alerts/warning-alert.component';
+import { SuccessAlertComponent } from './alerts/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
