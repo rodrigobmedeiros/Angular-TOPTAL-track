d732e2985175615106e4c4dc839181b88008bbd9
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-05-15 21:55:40 -0300

    Include an event emitter to emit the option clicked into header

--- angular-the-complete-guide-2023/course-project/src/app/header/header.component.ts
+++ angular-the-complete-guide-2023/course-project/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  @Output() featureOptions: EventEmitter<string> = new EventEmitter();
   collapsed = true;
+
+  onSelect(featureSelected: string): void {
+    this.featureOptions.emit(featureSelected);
+  }
 }
