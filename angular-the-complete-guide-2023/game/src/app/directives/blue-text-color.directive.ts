4b20380f2c5a82e078d9bf8b3333830974423681
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-05-17 06:38:12 -0300

    Use HostListener decorator to react an events from host template

--- angular-the-complete-guide-2023/game/src/app/directives/blue-text-color.directive.ts
+++ angular-the-complete-guide-2023/game/src/app/directives/blue-text-color.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBlueTextColor]'
@@ -7,8 +7,16 @@ export class BlueTextColorDirective implements OnInit {
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  @HostListener('mouseenter')
+  onMouseEnter() {
     this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'blue');
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'black');
   }
 
 }
