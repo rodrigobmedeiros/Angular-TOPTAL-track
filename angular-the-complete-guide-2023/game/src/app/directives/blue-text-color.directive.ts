1e90654ae9254d5c4b4ee230d62ad934a521d98e
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-05-18 06:28:59 -0300

    Use Host Binding decorator to bind an element ref property directly

--- angular-the-complete-guide-2023/game/src/app/directives/blue-text-color.directive.ts
+++ angular-the-complete-guide-2023/game/src/app/directives/blue-text-color.directive.ts
@@ -1,10 +1,18 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { 
+  Directive, 
+  ElementRef, 
+  HostBinding, 
+  HostListener, 
+  OnInit, 
+  Renderer2 
+} from '@angular/core';
 
 @Directive({
   selector: '[appBlueTextColor]'
 })
 export class BlueTextColorDirective implements OnInit {
-
+  @HostBinding('style.backgroundColor') backgroundColor: string = "transparent";
+  
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {}
@@ -12,11 +20,13 @@ export class BlueTextColorDirective implements OnInit {
   @HostListener('mouseenter')
   onMouseEnter() {
     this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'blue');
+    this.backgroundColor = "red";
   }
 
   @HostListener('mouseleave')
   onMouseLeave() {
     this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'black');
+    this.backgroundColor = "transparent";
   }
 
 }
