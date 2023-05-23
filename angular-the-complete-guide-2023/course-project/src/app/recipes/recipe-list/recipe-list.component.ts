7540fdc5ccb15dab11ca0434f311d95b94e8fe29
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-05-23 20:33:22 -0300

    Move recipes list to Recipe Service

--- angular-the-complete-guide-2023/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular-the-complete-guide-2023/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { RecipeService } from 'src/app/services/recipe.service';
 import { Recipe } from '../recipe.model'
 
 @Component({
@@ -9,19 +10,13 @@ import { Recipe } from '../recipe.model'
 export class RecipeListComponent {
   @Output() recipeSelected: EventEmitter<Recipe> = new EventEmitter();
 
+  recipes: Recipe[];
 
-  recipes: Recipe[] = [
-    new Recipe(
-      'pasta bolognesa', 
-      'delicious pasta with bolognesa souce', 
-      'https://realfood.tesco.com/media/images/1400x919-tomato-pasta-6a5a3c8e-f111-490d-805c-9b62fbec8691-0-1400x919.jpg'
-    ),
-    new Recipe(
-      'white rice', 
-      'delicious white rice with garlic', 
-      'https://static.itdg.com.br/images/1200-675/21fd76be3b29c3290859eda5220e0e32/323683-original.jpg'
-    )
-  ];
+  constructor(private recipeService: RecipeService) {}
+
+  ngOnInit(): void {
+    this.recipes = this.recipeService.recipes;
+  }
 
   onSelect(recipe: Recipe) {
     this.recipeSelected.emit(recipe);
--- angular-the-complete-guide-2023/course-project/src/app/services/recipe.service.ts
+++ angular-the-complete-guide-2023/course-project/src/app/services/recipe.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from "@angular/core";
+import { Recipe } from "../recipes/recipe.model";
 
 @Injectable({
     providedIn: 'root'
 })
 export class RecipeService {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'pasta bolognesa', 
+      'delicious pasta with bolognesa souce', 
+      'https://realfood.tesco.com/media/images/1400x919-tomato-pasta-6a5a3c8e-f111-490d-805c-9b62fbec8691-0-1400x919.jpg'
+    ),
+    new Recipe(
+      'white rice', 
+      'delicious white rice with garlic', 
+      'https://static.itdg.com.br/images/1200-675/21fd76be3b29c3290859eda5220e0e32/323683-original.jpg'
+    )
+  ];
 }
