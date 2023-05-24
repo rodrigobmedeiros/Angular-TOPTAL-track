b8cea0d5c38ae06d59c7429464a1490c55fdf7ea
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-05-23 22:14:51 -0300

    Add ingredients to recipe model and update where needed

--- angular-the-complete-guide-2023/course-project/src/app/recipes/recipe.model.ts
+++ angular-the-complete-guide-2023/course-project/src/app/recipes/recipe.model.ts
@@ -1,11 +1,16 @@
-export class Recipe {
-    public name: string;
-    public description: string;
-    public imagePath: string;
+import { Ingredient } from "../shared/ingredient.model";
 
-    constructor(name: string, description: string, imagePath: string) {
+export class Recipe {
+
+    constructor(
+        public name: string, 
+        public description: string, 
+        public imagePath: string, 
+        public ingredients: Ingredient[]
+    ) {
         this.name = name;
         this.description = description;
         this.imagePath = imagePath;
+        this.ingredients = ingredients
     }
 }
--- angular-the-complete-guide-2023/course-project/src/app/services/recipe.service.ts
+++ angular-the-complete-guide-2023/course-project/src/app/services/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter, Injectable, Output } from "@angular/core";
 import { Recipe } from "../recipes/recipe.model";
+import { Ingredient } from "../shared/ingredient.model"
 
 @Injectable({
     providedIn: 'root'
@@ -11,12 +12,20 @@ export class RecipeService {
     new Recipe(
       'pasta bolognesa', 
       'delicious pasta with bolognesa souce', 
-      'https://realfood.tesco.com/media/images/1400x919-tomato-pasta-6a5a3c8e-f111-490d-805c-9b62fbec8691-0-1400x919.jpg'
+      'https://realfood.tesco.com/media/images/1400x919-tomato-pasta-6a5a3c8e-f111-490d-805c-9b62fbec8691-0-1400x919.jpg',
+      [
+        new Ingredient('pasta', 1),
+        new Ingredient('tomato souce', 1)
+      ]
     ),
     new Recipe(
       'white rice', 
       'delicious white rice with garlic', 
-      'https://static.itdg.com.br/images/1200-675/21fd76be3b29c3290859eda5220e0e32/323683-original.jpg'
+      'https://static.itdg.com.br/images/1200-675/21fd76be3b29c3290859eda5220e0e32/323683-original.jpg',
+      [
+        new Ingredient('rice', 1),
+        new Ingredient('garlic', 2)
+      ]
     )
   ];
 
