84587d0458e2ac07fe6895dbdde8a5f8c314741d
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-06-03 05:10:01 -0300

    Add routes for edit component and implement route param retrieving

--- angular-the-complete-guide-2023/course-project/src/app/app-routing.module.ts
+++ angular-the-complete-guide-2023/course-project/src/app/app-routing.module.ts
@@ -6,12 +6,15 @@ import { RecipesComponent } from './recipes/recipes.component'
 import { NoRecipeSelectedComponent } from './recipes/recipe-list/no-recipe-selected/no-recipe-selected.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 const routes: Route[] = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: 'recipes', component: RecipesComponent, children: [
     { path: '', component: NoRecipeSelectedComponent },
-    { path: ':id', component: RecipeDetailComponent}
+    { path: 'new', component: RecipeEditComponent },
+    { path: ':id', component: RecipeDetailComponent },
+    { path: ':id/edit', component: RecipeEditComponent }
   ]},
   { path: 'shopping-list', component: ShoppingListComponent}
 ]
--- angular-the-complete-guide-2023/course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ angular-the-complete-guide-2023/course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
   styleUrls: ['./recipe-edit.component.css']
 })
-export class RecipeEditComponent {
+export class RecipeEditComponent implements OnInit{
+  id: number;
+  editMode: boolean = false;
+  
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params.id;
+      this.editMode = Boolean(params.id);
+      console.log(this.editMode);
+    })
+  }
 }
