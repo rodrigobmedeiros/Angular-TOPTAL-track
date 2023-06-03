ce9989ed8896c4b744b22d32bb7a92f46a7ff93d
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-06-03 18:24:02 -0300

    Create a custom observable

--- angular-the-complete-guide-2023/obs-01-start/src/app/home/home.component.ts
+++ angular-the-complete-guide-2023/obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Observable, Observer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -13,8 +13,18 @@ export class HomeComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    this.subscribe = interval(1000).subscribe((count) => {
-      console.log(count);
+    // this.subscribe = interval(1000).subscribe((count) => {
+    //   console.log(count);
+    // })
+    let count = 0;
+    const customIntervalObserver = Observable.create((observer: Observer<number>) => {
+      setInterval(() => {
+        observer.next(count);
+        count++
+      }, 1000);
+    })
+    this.subscribe = customIntervalObserver.subscribe((data) => {
+      console.log(data);
     })
   }
 
