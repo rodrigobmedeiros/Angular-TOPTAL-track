b6faa4d3323a6f99083dc7af9cb617daa95ea23a
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-06-08 16:35:15 -0300

    Solve form exercise

---
+++ angular-the-complete-guide-2023/form-exercise/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('form') form: NgForm | null = null;
+  title = 'form-exercise';
+  subscriptions = [
+    "Basic",
+    "Advanced",
+    "Pro"
+  ];
+  defaultSubscription="Advanced";
+  submitted: boolean = false;
+  userData = {
+    email: '',
+    subscription: ''
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.userData.email = this.form!.value.email;
+    this.userData.subscription = this.form!.value.subscription;
+    this.form!.reset();
+  }
+}
