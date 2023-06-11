46ae7855a2a7eb68f44e88b4d5ff7e377ab85a64
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-06-11 08:28:22 -0300

    Implement a custom validator

--- angular-the-complete-guide-2023/forms-reactive-start/src/app/app.component.ts
+++ angular-the-complete-guide-2023/forms-reactive-start/src/app/app.component.ts
@@ -9,11 +9,12 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   userForm: FormGroup;
+  notPermitedNames: string[] = ['Rodrigo', 'Renata'];
 
   ngOnInit() {
     this.userForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }),      
       'gender': new FormControl('male'),
@@ -32,5 +33,12 @@ export class AppComponent implements OnInit {
   onAddcontrols() {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.userForm.get('hobbies')).push(control);
+  }
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if (this.notPermitedNames.indexOf(control.value) !== -1) {
+      return {forbiddenName: true};
+    }
+    return null // Or omitted
   }
 }
