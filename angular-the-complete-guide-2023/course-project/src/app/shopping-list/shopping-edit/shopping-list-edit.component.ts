635414810fd933078bcfc3351fabfd0783169cd0
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-06-17 05:21:42 -0300

    Use template driven approach to make the shopping list form an angular form

--- angular-the-complete-guide-2023/course-project/src/app/shopping-list/shopping-edit/shopping-list-edit.component.ts
+++ angular-the-complete-guide-2023/course-project/src/app/shopping-list/shopping-edit/shopping-list-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ShoppingListService } from 'src/app/services/shopping-list.service';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 
@@ -8,15 +9,12 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent {
-  @ViewChild('inputName', {static: false}) nameField: ElementRef;
-  @ViewChild('inputAmount', {static: false}) amountField: ElementRef;
 
   constructor(private shoppingListService: ShoppingListService) {}
   
-  public onAdd() {
-    const name = this.nameField.nativeElement.value;
-    const amount = this.amountField.nativeElement.value;
-    const ingredient = new Ingredient(name, +amount);
+  public onAdd(form: NgForm) {
+    const formValue = form.value;
+    const ingredient = new Ingredient(formValue.name, formValue.amount);
     this.shoppingListService.addIngredient(ingredient);
   }
 
