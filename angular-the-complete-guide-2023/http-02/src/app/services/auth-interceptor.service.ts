ea89b53d9fb71a29b6fe1f0e8a48a668c1381aee
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-07-02 07:13:30 -0300

    Modify the request and return the modified one

--- angular-the-complete-guide-2023/http-02/src/app/services/auth-interceptor.service.ts
+++ angular-the-complete-guide-2023/http-02/src/app/services/auth-interceptor.service.ts
@@ -3,6 +3,7 @@ import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http"
 export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         console.log('Passing through interceptor...');
-        return next.handle(req)
+        const modifiedReq = req.clone({headers: req.headers.append('auth', 'xyz')});
+        return next.handle(modifiedReq)
     }
 }
