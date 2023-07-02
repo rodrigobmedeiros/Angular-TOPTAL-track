e442bba481a53ac2212ac3456759898ee9e54f30
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-07-02 07:22:46 -0300

    Implement Body manipulation by interceptors

--- angular-the-complete-guide-2023/http-02/src/app/services/auth-interceptor.service.ts
+++ angular-the-complete-guide-2023/http-02/src/app/services/auth-interceptor.service.ts
@@ -1,9 +1,16 @@
-import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { tap } from "rxjs";
 
 export class AuthInterceptorService implements HttpInterceptor {
-    intercept(req: HttpRequest<any>, next: HttpHandler) {
-        console.log('Passing through interceptor...');
-        const modifiedReq = req.clone({headers: req.headers.append('auth', 'xyz')});
-        return next.handle(modifiedReq)
-    }
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    console.log('Passing through interceptor...');
+    const modifiedReq = req.clone({headers: req.headers.append('auth', 'xyz')});
+    return next.handle(modifiedReq).pipe(tap((event) => {
+      console.log(event);
+      if (event.type === HttpEventType.Response) {
+        console.log('Response has arrived...')
+        console.log(event.body);
+      }
+    }))
+  }
 }
