2c2c3d065f4f99b127c7aaf96dd8a7c81e7e2555
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-07-04 07:43:21 -0300

    Start the service to connect with the firebase database

--- angular-the-complete-guide-2023/course-project/src/app/app.module.ts
+++ angular-the-complete-guide-2023/course-project/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DropdownDirective } from './shared/dropdown.directive';
 import { AppRoutingModule } from './app-routing.module';
 import { NoRecipeSelectedComponent } from './recipes/recipe-list/no-recipe-selected/no-recipe-selected.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     ReactiveFormsModule,
     AppRoutingModule
   ],
